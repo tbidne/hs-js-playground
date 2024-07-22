c1fe54df2c9c31094845525a08c74bc82903749c
Tommy Bidne <tbidne@protonmail.com>
2024-07-22 16:26:39 +1200

    Initial commit

---
+++ README.md
@@ -0,0 +1,9 @@
+<div align="center">
+
+# hs-js-playground
+
+[![MIT](https://img.shields.io/github/license/tbidne/hs-js-playground?color=blue)](https://opensource.org/licenses/MIT)
+
+</div>
+
+---
---
+++ flake.nix
@@ -0,0 +1,53 @@
+{
+  description = "A Template for Haskell Packages";
+  inputs.flake-parts.url = "github:hercules-ci/flake-parts";
+  inputs.nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+  inputs.nix-hs-utils.url = "github:tbidne/nix-hs-utils";
+  outputs =
+    inputs@{
+      flake-parts,
+      nixpkgs,
+      nix-hs-utils,
+      self,
+    }:
+    flake-parts.lib.mkFlake { inherit inputs; } {
+      perSystem =
+        { pkgs, ... }:
+        let
+          hlib = pkgs.haskell.lib;
+          ghc-version = "ghc982";
+          # override packages set rather than developPackage's overrides so
+          # we can use the same overlay with nix build, dev shell, and apps.
+          compiler = pkgs.haskell.packages."${ghc-version}".override { overrides = _: _: { }; };
+          compilerPkgs = {
+            inherit compiler pkgs;
+          };
+
+          mkPkg =
+            returnShellEnv:
+            nix-hs-utils.mkHaskellPkg {
+              inherit compiler pkgs returnShellEnv;
+              name = "hs-js-playground";
+              root = ./.;
+            };
+        in
+        {
+          packages.default = mkPkg false;
+          devShells = {
+            default = mkPkg true;
+          };
+
+          apps = {
+            format = nix-hs-utils.format compilerPkgs;
+            lint = nix-hs-utils.lint compilerPkgs;
+            lintRefactor = nix-hs-utils.lintRefactor compilerPkgs;
+          };
+        };
+      systems = [
+        "aarch64-darwin"
+        "aarch64-linux"
+        "x86_64-darwin"
+        "x86_64-linux"
+      ];
+    };
+}
